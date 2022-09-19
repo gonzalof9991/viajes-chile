0a8d5c9d3711a2569fbf2347c95c3e55e19abc9d
Gonzalo Eduardo Rojas Falfan <80917894+gonzalor9991@users.noreply.github.com>
2022-09-19 20:58:42 -0300

    Integrando readme - footer - github pages

---
+++ assets/js/jquery.js
@@ -0,0 +1,19 @@
+
+$(document).ready( function(){
+    // Boton Email
+   $("#buttonSend").click(() => {
+    alert("El correo fue enviado correctamente...");
+   })
+
+
+  // Boton Email
+  $(".card-title").click(() => {
+    $(".card-text").toggle("slow");
+   })
+
+
+
+   function changeTitle(){
+    alert('Acaa')
+   }
+});
